c7a1f45b8baf03ea98de3ec7a2ee727578b15964
core <943134861@qq.com>
2025-12-02 11:20:27 +0800

    Update traffic light position in main.ts

--- electron/main.ts
+++ electron/main.ts
@@ -55,7 +55,7 @@ function createWindow() {
           },
         }
       : {}),
-    trafficLightPosition: { x: 10, y: 10 },
+    trafficLightPosition: { x: 15, y: 10 },
     webPreferences: {
       preload: path.join(__dirname, "preload.cjs"),
       webSecurity: true,
